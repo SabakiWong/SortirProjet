33814d84c81bdda5d576cc65bf0ff6b8ba34b6b5
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2021-09-22 09:43:58 +0200

    Correction Système de registre et login (sans finir)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController {
 
     /**
-     * @Route (/create , name="sortie_create")
+     * @Route("/create" , name="sortie_create")
      */
     public function create(){
 
